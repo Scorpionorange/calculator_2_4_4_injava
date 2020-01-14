7b154682ce9dbd25cdc75b9c1a7763743b9fa834
ScorpionOrange <scorpionorange@126.com>
2020-01-15 01:42:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/calculator_2_4_4_injava/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.calculator_2_4_4_injava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+}
