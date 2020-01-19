9552c8aed5a1918911963008c9e161bd2c3fc127
ScorpionOrange <scorpionorange@126.com>
2020-01-19 14:02:09 +0800

    update at 2020.1.19.14:01pm

--- app/src/main/java/com/example/calculator_2_4_4_injava/MainActivity.java
+++ app/src/main/java/com/example/calculator_2_4_4_injava/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button btC = null, btDel = null, btEqual = null,
+            bt7 = null, bt8 = null, bt9 = null, btPlus = null,
+            bt4 = null, bt5 = null, bt6 = null, btMinus=null,
+            bt1 = null, bt2 = null, bt3 = null, btMultiple = null,
+            btPlusOrMinus = null, bt0 = null, btPoint = null, btdiv = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
